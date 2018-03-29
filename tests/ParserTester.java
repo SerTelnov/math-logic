5da3dd8a60713f88d4f898da41afeab33721295f
hangedplane <hangedplane@gmail.com>
2018-03-29 22:34:07 +0300

    add test folder

--- tests/ParserTester.java
+++ tests/ParserTester.java
@@ -1,9 +1,10 @@
-package tests;
-
 import org.junit.jupiter.api.Test;
 import parser.ExpressionParser;
+import parser.expressions.Expression;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by Telnov Sergey on 17.03.2018.
@@ -11,21 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTester {
     @Test
     public void test01() {
-        assertEquals("(->,a,b)", new ExpressionParser().parse("a->b").toStr());
+        assertEquals("(->,A,B)", new ExpressionParser().parse("A->B").toTree());
     }
 
     @Test
     public void test02() {
         assertEquals(
                 "(->,P,(->,(!QQ),(|,(&,(!R10),S),(&,(&,(!T),U),V))))",
-                new ExpressionParser().parse("P->!QQ->!R10&S|!T&U&V").toStr());
+                new ExpressionParser().parse("P->!QQ->!R10&S|!T&U&V").toTree());
     }
 
     @Test
     public void test03() {
         assertEquals(
                 "(->,(&,(!A),(!B)),(!(|,A,B)))",
-                new ExpressionParser().parse("!A&!B->!(A|B)").toStr()
+                new ExpressionParser().parse("!A&!B->!(A|B)").toTree()
         );
     }
 }
